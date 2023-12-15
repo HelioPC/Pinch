d3a51d4d48ced0b66c8dcca7aa77de5698ab8e19
Eliúde Vemba <sweeteliude@gmail.com>
2023-12-15 02:18:21 +0100

    Step 01: Initial setup

---
+++ Pinch/Screen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Eliude Vemba on 15/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
