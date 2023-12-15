10753b9e8a28517984d98a6a394ee49bc8fc5854
Eliúde Vemba <sweeteliude@gmail.com>
2023-12-15 01:57:27 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Eliude Vemba on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
