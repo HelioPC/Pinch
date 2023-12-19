75624aa12d9337ff752cde0abf9915cc4215666c
Eliúde Vemba <sweeteliude@gmail.com>
2023-12-19 03:57:33 +0100

    Step 06: Add a drawer 01/02

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
+    @State private var isDrawerOpen: Bool = false
     
 //    MARK: - FUNCTIONS
     
@@ -135,6 +136,33 @@ struct ContentView: View {
                     .opacity(isAnimating ? 1 : 0)
                 }
                 .padding(.bottom, 30)
+            }
+//          MARK: - DRAWER
+            .overlay(alignment: .topTrailing) {
+                HStack {
+//                  MARK: - DRAWER HANDLE
+                    Image(systemName: isDrawerOpen ? "chevron.compact.right" : "chevron.compact.left")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 40)
+                        .padding(8)
+                        .foregroundStyle(.secondary)
+                        .onTapGesture(perform: {
+                            withAnimation(.easeOut) {
+                                isDrawerOpen.toggle()
+                            }
+                        })
+                    
+//                  MARK: - DRAWER THUMBNAILS
+                    Spacer()
+                }
+                .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 12))
+                .opacity(isAnimating ? 1 : 0)
+                .frame(width: 260)
+                .padding(.top, UIScreen.main.bounds.height / 12)
+                .offset(x: isDrawerOpen ? 20 : 215)
             }
         }
     }
